950f677cfc00afd43c57fa0e25346d383fa9f939
Josh Jenkin <josh.jenkin@live.com>
2023-05-24 15:05:42 -0700

    Added new main page info.

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -9,12 +9,15 @@ export default function Navbar(props) {
             <div className="flex-1">
                 <Link href={'/'}><img src={logo.image.url} alt={logo.title} className='h-20' /></Link>
             </div>
-            <div className="flex-none">
+            <div className="">
                 <ul className="menu menu-horizontal px-1">
-                    <li className="text-black">
-                        <a href="https://www.charleescomfortkitchen.com/" target='_blank'>Charlees Comfort Kitchen</a>
+                    <li className="text-black text-center">
+                    <motion.button
+                        whileHover={{ scale: 1.1 }}
+                        className="glass text-black">
+                        <a className="break-all text-base leading-tight" href="https://www.charleescomfortkitchen.com/" target='_blank'>Charlees<br/> Comfort<br/> Kitchen</a>
+                    </motion.button>
                     </li>
-                    
                 </ul>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Footer from '../components/Footer/Footer'
 
 export default function Home(props) {
   const headers = props.headers.data.headersCollection.items
+  console.log(headers[0].paragraph)
   const logo = props.logo.data.logo
   const background = props.background.data.backgroundImage.background.url
   const menu = props.menu
@@ -21,7 +22,7 @@ export default function Home(props) {
       <main className='bg-gray-900'>
         <div className="hero min-h-screen" style={{ backgroundImage: `url(${background})` }}>
           <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
+          <div className="hero-content text-center text-neutral-content flex-col my-44">
             <div className="max-w-md text-white">
               <h1 className="mb-5 text-7xl font-bold">{headers[0].firstHeader}</h1>
               <p className="mb-5 text-2xl">{headers[0].description}</p>
@@ -31,7 +32,19 @@ export default function Home(props) {
                 <a href='https://charlees-heat-at-home.square.site/' target='_blank'>Order Now</a>
               </motion.button>
             </div>
+            <p className="mb-5 text-sm text-white my-10 bg-gray-900 bg-opacity-50 rounded-xl p-3">
+            Our mission at Charlee’s is to provide high quality food at affordable prices. The Heat at Home branch of Charlee’s provides much larger portions with savings to match. Everything is sold cold with simple heating instructions. In order to ensure our high standards all Heat at Home items are made to order so please place your order 20-30 minutes before pickup.
+            <br/><br/>
+            EBT is accepted on all (in person) Heat at Home purchases. We are unable to accept EBT online so if that is your preferred purchasing method please call in your order and pay when you pick it up.
+            <br/><br/>
+            <motion.button
+                whileHover={{ scale: 1.1 }}
+                className="rounded-lg glass text-white">
+                  <a href='tel:+4352263635'>(435)226-3635</a>
+              </motion.button>
+            </p>
           </div>
+
         </div>
         <div className='container mx-auto py-24'>
           <Menu menu={menu} />
